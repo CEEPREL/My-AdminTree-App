628a10f199d984bd555e07c6f251f355a60589c5
CEEPREL <ceeprel3@gmail.com>
2025-05-01 19:38:55 +0100

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
